6b38aaeebd6e2c29ccf3177c2bf9e47f18f30800
pppppplk <58656331+pppppplk@users.noreply.github.com>
2021-04-26 17:58:02 +0300

    Delete testing directory

--- testing/src/main/java/project/spring/repo/PerformanceRepo.java
+++
@@ -1,7 +0,0 @@
-package project.spring.repo;
-
-import project.spring.models.Performance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerformanceRepo extends JpaRepository<Performance, Long> {
-}
--- testing/src/main/java/project/spring/repo/SeatRepo.java
+++
@@ -1,7 +0,0 @@
-package project.spring.repo;
-
-import project.spring.models.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatRepo extends JpaRepository<Seat, Long> {
-}
